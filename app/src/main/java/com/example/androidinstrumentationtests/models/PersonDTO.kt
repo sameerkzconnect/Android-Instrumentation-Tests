c5007ec3a6870bcc88b363e04fa2f7396139a7a3
sameerkzconnect <sameerkzconnect@gmail.com>
2024-08-10 10:16:57 +0530

    Data Setup
    1.Junit added
    2.Models and repository created

---
+++ app/src/main/java/com/example/androidinstrumentationtests/models/PersonDTO.kt
@@ -0,0 +1,3 @@
+package com.example.androidinstrumentationtests.models
+
+data class PersonDTO(val name:String,val language:String)
---
+++ app/src/main/java/com/example/androidinstrumentationtests/repository/PersonRespository.kt
@@ -0,0 +1,43 @@
+package com.example.androidinstrumentationtests.repository
+
+import android.content.Context
+import com.example.androidinstrumentationtests.models.PersonDTO
+import com.google.gson.Gson
+
+
+class PersonRespository (){
+
+    var personList = emptyArray<PersonDTO>()
+    var currentPersionIndex = 0
+
+    fun populateQuoteFromAssets(context: Context, fileName: String){
+        val inputStream = context.assets.open(fileName)
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        personList = gson.fromJson(json, Array<PersonDTO>::class.java)
+    }
+    fun populateQuotes(quotes: Array<PersonDTO>){
+        personList = quotes
+    }
+
+    fun getCurrentQuote(): PersonDTO {
+        return personList[currentPersionIndex]
+    }
+
+    fun getNextQuote(): PersonDTO {
+        if (currentPersionIndex == personList.size - 1) return personList[currentPersionIndex]
+        return personList[++currentPersionIndex]
+    }
+
+    fun getPreviousQuote(): PersonDTO {
+        if (currentPersionIndex == 0) return personList[currentPersionIndex]
+        return personList[--currentPersionIndex]
+    }
+
+
+
+}
